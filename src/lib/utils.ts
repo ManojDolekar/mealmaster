fcd9eee15a44ef9d59d87e1b3695116f5889169e
ManojDolekar <manojdolekar9@gmail.com>
2025-10-01 12:25:55 +0530

    deploy

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import {  clsx } from "clsx";
+// import {  clsx } from "clsx";
+// import { twMerge } from "tailwind-merge";
+
+// export function cn (...inputs: ClassValue[]) {
+//   return twMerge(clsx(inputs));
+// }
+
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn (...inputs: ClassValue[]) {
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
