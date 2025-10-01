c2ee4978dac1222378c04fd9ee5cb12ee8ca08fe
ManojDolekar <manojdolekar9@gmail.com>
2025-10-01 11:30:48 +0530

    deploy

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,24 +1,3 @@
-// import React from 'react'
-// import { Link ,useNavigate} from 'react-router-dom'
-// import Logo from './Logo/Logo';
-// import { faUtensils } from '@fortawesome/free-solid-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-
-// function Header() {
-//   const navigate=useNavigate();
-
-//   return (
-//     <section className='flex w-full p-2 bg-[#e8ece8] justify-between items-center flex-row '>
-//         <div onClick={ ()=>navigate('/')} className='hover:rotate-2 hover:scale-105 duration-200 mx-6 cursor-pointer'> <Logo/></div>
-//         <div className='mx-6  visited:text-amber-600' >
-//           <Link to='random-meal' className=' group items-center flex gap-2 hover:text-amber-600'><FontAwesomeIcon className=" group-hover:scale-125 duration-200 group-hover:text-amber-600 text-black/50 text-xl" icon={faUtensils}  /> Browse random meal</Link> 
-//         </div>
-//     </section>
-//   )
-// }
-
-// export default Header
 
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,20 +1,3 @@
-// import React from 'react'
-// import Banner from './HomePage-Componets/Banner'
-// import bannerImg from '../Images/pexels-adonyi-foto-2064359.jpg'
-// import Slider from './Slider/Slider'
-
-// function Home() {
-//   return (
-//     <div style={{backgroundImage:`url(${bannerImg})`}} className=' bg-fixed bg-cover'>
-//         <Banner/>
-//         <div className=' bg-[#e8ece8]'>
-//         <Slider/>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default Home
 
 import Banner from './HomePage-Componets/Banner';
 import bannerImg from '../Images/pexels-adonyi-foto-2064359.jpg';
